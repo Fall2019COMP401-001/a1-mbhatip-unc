acf7cc8d8503b5557193040fabf681a27f5e3750
Memohat <49382701+Memohat@users.noreply.github.com>
2019-08-29 18:53:45 -0400

    Started work on A1 Adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,40 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		// Declaring variables for items
+		int numItems = scan.nextInt();
+		String[] nameItems = new String[numItems];
+		double[] priceItems = new double[numItems];
+		
+		// loop for item info
+		for (int i = 0; i < numItems; i++) {
+			nameItems[i] = scan.next();
+			priceItems[i] = scan.nextDouble();
+		}
+		
+		// Declaring variables for customers
+		int numCust = scan.nextInt();
+		String[] name = new String[numCust];
+		int[] numBought = new int[numCust];
+		int[] quantityC; 
+		String[] nameItemsC;
+		
+		// Loop for customer info
+		for (int i = 0; i < numCust; i++) {
+			name[i] = scan.next() + " " + scan.next();
+			numBought[i] = scan.nextInt();
+			
+			// Initialize customer item variables
+			quantityC = new int[numBought[i]];
+			nameItemsC = new String[numBought[i]];
+			
+			// Loop for each item bought by single customer
+			for (int j = 0; j < numBought[i]; j++) {
+				quantityC[j] = scan.nextInt();
+				nameItemsC[j] = scan.next();
+				
+			}
+		}
+		
 	}
 }
